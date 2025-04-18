705950b4b143888100cbdbe7009c856d57cf7717
Jacob Harvey-Hale <84041117+Spookzyy@users.noreply.github.com>
2025-04-18 22:35:04 +0100

    Update and rename ui-install.sh to gaming.sh

---
+++ gaming.sh
@@ -0,0 +1,7 @@
+## confirtmation prmpt to install
+read -p "this scipt only install things for gaming only!! Are you sure you want to continue? y/n : " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
