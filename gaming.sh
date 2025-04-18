32d13dd02d83ac25493dad0a96324ade1056e217
Jacob Harvey-Hale <84041117+Spookzyy@users.noreply.github.com>
2025-04-18 22:37:48 +0100

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -5,3 +5,17 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
+
+## ask to instll discord
+read -p " install discord? y/n : " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+sudo pacman -S discord
+
+##clear
+clear
+
+
