98c306998ed4a46c9928f49ed5ce7f9a1f4045bb
Jacob Harvey-Hale <84041117+Spookzyy@users.noreply.github.com>
2025-04-18 22:44:48 +0100

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -6,6 +6,9 @@ then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
 
+##clear for questions 
+clear
+
 ## ask to instll discord
 read -p "install discord? y/n : " -n 1 -r
 echo    # (optional) move to a new line
@@ -13,9 +16,45 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
 fi
+
 sudo pacman -S discord
+
+##clear 
+clear
+
+read -p "install cartridges? y/n : " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+sudo pacman -S cartridges
 
 ##clear
 clear
 
+read -p "install lutris? y/n : " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+sudo pacman -S lutris
+##clear 
+clear
+
+read -p "install goverlay dxvk and steam? MAKE SURE MULTIB IS ENABLED OR STEAM WONT DOWNLOAD. y/n : " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
+sudo pacman -S goverlay steam
+yay -S dxvk
+
+#clear
+clear
 
